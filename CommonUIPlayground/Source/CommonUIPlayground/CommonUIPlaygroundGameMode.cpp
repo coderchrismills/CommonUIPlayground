e609a88879cd6b5a38b7fafc4043a3c1b332f4e5
coderchrismills <coderchrismills@gmail.com>
2022-10-06 16:57:32 -0700

    Initial commit
    Adding initial files with draggable UI element.

---
+++ CommonUIPlayground/Source/CommonUIPlayground/CommonUIPlaygroundGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CommonUIPlaygroundGameMode.h"
+#include "CommonUIPlaygroundCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ACommonUIPlaygroundGameMode::ACommonUIPlaygroundGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
